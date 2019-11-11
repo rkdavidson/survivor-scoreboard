df08f01e7416fe88791acd447d2e59fd2b9ba21e
Ryan Davidson <ryan@rkdavidson.com>
2019-11-10 23:03:08 -0500

    Updates

---
+++ precache-manifest.fa9baa880ac484ee3f6373550464025f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e83c66043c82e63e9dbb897588f550c",
+    "url": "/survivor-scorecard/index.html"
+  },
+  {
+    "revision": "1ba5c2bdcd63e45426ec",
+    "url": "/survivor-scorecard/static/css/main.dcb08a8b.chunk.css"
+  },
+  {
+    "revision": "d2ee0a0b5c6e44889e83",
+    "url": "/survivor-scorecard/static/js/2.09ae0715.chunk.js"
+  },
+  {
+    "revision": "1ba5c2bdcd63e45426ec",
+    "url": "/survivor-scorecard/static/js/main.2167d875.chunk.js"
+  },
+  {
+    "revision": "09f2d32d5ca4146901d7",
+    "url": "/survivor-scorecard/static/js/runtime-main.bec31db6.js"
+  }
+]);
