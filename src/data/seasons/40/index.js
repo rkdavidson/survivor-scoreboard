20c7bf35333cd28e5b2a4e0b303b023db3eb6bb4
Ryan Davidson <ryan@rkdavidson.com>
2020-04-15 21:14:16 -0400

    Update for April 8th episode 9

--- src/data/seasons/40/index.js
+++ src/data/seasons/40/index.js
@@ -2,9 +2,9 @@ import season from './season';
 import cast from './cast';
 
 // ⭐ Things that change every week
-import episode from './episodes/episode08';
-import homeGame from './games/home/home08';
-import workGame from './games/work/work08';
+import episode from './episodes/episode09';
+import homeGame from './games/home/home09';
+import workGame from './games/work/work09';
 
 export default {
   season,
