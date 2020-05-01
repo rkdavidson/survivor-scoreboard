f014ce6a166b9b8c3752d9ad0b796aa5d866138f
Ryan Davidson <ryan@rkdavidson.com>
2020-05-01 14:35:37 -0400

    Add episode 11 from episode on 4/22

--- src/data/seasons/40/index.js
+++ src/data/seasons/40/index.js
@@ -2,9 +2,9 @@ import season from './season';
 import cast from './cast';
 
 // ⭐ Things that change every week
-import episode from './episodes/episode10';
-import homeGame from './games/home/home10';
-import workGame from './games/work/work10';
+import episode from './episodes/episode11';
+import homeGame from './games/home/home11';
+import workGame from './games/work/work11';
 
 export default {
   season,
